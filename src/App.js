fdd2d01360c9065aaef3b44ff869f0c07f69b8c6
williamvelichko <williamvelichko2003@gmail.com>
2021-12-17 10:18:05 -0800

    made a pizza size dropdown

--- src/App.js
+++ src/App.js
@@ -6,10 +6,44 @@ import "./App.css";
 const info = {
   picture:
     "https://cdn.pixabay.com/photo/2020/05/17/04/22/pizza-5179939__480.jpg",
+  picture2:
+    "https://media.istockphoto.com/photos/bakery-chef-prepare-pizza-picture-id1291299956?b=1&k=20&m=1291299956&s=170667a&w=0&h=Ys_FLtdY0Uzc7yTQl6JzvCHTQ3eRAuqNNU4x8EX1FB8=",
+};
+const initialFormValues = {
+  size: "",
+  redSauce: false,
+  garlicRanch: false,
+  bbqSauce: false,
+  spinachRanch: false,
+
+  pepperoni: false,
+  sausage: false,
+  canadianBacon: false,
+  spicyItalianSausage: false,
+  grilledChicken: false,
+  onions: false,
+  greenPepper: false,
+  dicedTomatos: false,
+  blackOlives: false,
+  roastedGarlic: false,
+  artichokeHearts: false,
+  threeCheese: false,
+  pineapple: false,
+  extraCheese: false,
+
+  glutenFree: false,
+  extraStuff: "",
 };
 
 const App = () => {
   const [users, setUsers] = useState(info);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState("");
+  const [disabled, setDisabled] = useState(true);
+
+  const inputChange = (name, value) => {};
+
+  const formSubmit = () => {};
 
   return (
     <div className="order-pizza">
@@ -26,7 +60,14 @@ const App = () => {
         <Home users={users} />
       </Route>
       <Route path="/pizza">
-        <PizzaForm />
+        <PizzaForm
+          users={users}
+          value={formValues}
+          change={inputChange}
+          submit={formSubmit}
+          disabled={disabled}
+          errors={formErrors}
+        />
       </Route>
     </div>
   );
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,5 +1,31 @@
 import React from "react";
 
-export default function PizzaForm() {
-  return <div></div>;
+export default function PizzaForm(props) {
+  const { value, submit, change, disabled, errors } = props;
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+  const onChange = (evt) => {};
+  return (
+    <div className="pizza-form" onSubmit={onSubmit}>
+      <img src={props.users.picture2} alt="users" />
+      <div className="name-input">
+        <h3>Build Your Own Pizza</h3>
+        <div className="size-dropdown">
+          <label>
+            <h4>Choice of Size</h4>
+            <select value={value.size} onChange={onChange} name="size">
+              <option value="">--select size--</option>\
+              <option value="small">small</option>
+              <option value="medium">medium</option>
+              <option value="large">large</option>
+              <option value="x-large">x-large</option>
+            </select>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 }
