c4479e0e45db8efff2e583b53a1cce56bf2c517b
williamvelichko <williamvelichko2003@gmail.com>
2021-12-16 18:00:28 -0800

    connected my home components with app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-
+import { Link, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="order-pizza">
+      <header>
+        <h1>Lambda Eats</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/pizza">Order</Link>
+        </nav>
+      </header>
+
+      <Route path="/">
+        <Home />
+      </Route>
+    </div>
   );
 };
 export default App;
